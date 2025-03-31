dd523cd68fb3e1932ff3fc6ff364810bf75e6127
Nguyễn Bá Hiệp <your_gitlab_email@example.com>
2025-03-31 15:06:07 +0700

    ket noi toi db

---
+++ doanwebthoitrang/src/main/java/com/example/doanwebthoitrang/DoanwebthoitrangApplication.java
@@ -0,0 +1,13 @@
+package com.example.doanwebthoitrang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoanwebthoitrangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoanwebthoitrangApplication.class, args);
+	}
+
+}
